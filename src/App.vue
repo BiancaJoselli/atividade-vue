a800b3e7484cc45d8ae136c7c2af7073b32923e2
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-04-24 08:13:39 -0300

    FEAT:  Making some changes to the links

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <script setup>
-import { computed, reactive, watch } from 'vue'
-import '@fortawesome/fontawesome-free/css/all.css'
+import { computed, reactive } from 'vue';
 
-const state = reactive({
-    searchTerm: ''
-})
+const livros = [
+    {
+        id: 1,
+        titulo: 'Harry Potter e a Pedra Filosofal',
+        autor: 'J. K. Rowling',
+        preco: 50.99,
+        capa: '',
+    },
+    {
+        id: 2,
+        titulo: 'Harry Potter e a Câmara Secreta',
+        autor: 'J. K. Rowling',
+        preco: 45.90,
+        capa: '',
+    },
+    {
+        id: 3,
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'J. K. Rowling',
+        preco: 40.70,
+        capa: '',
+    },
+    {
+        id: 4,
+        titulo: 'Harry Potter e o Cálice de Fogo',
+        autor: 'J. K. Rowling',
+        preco: 60.89,
+        capa: '',
+    },
+    {
+        id: 5,
+        titulo: 'Harry Potter e a Ordem da Fênix',
+        autor: 'J. K. Rowling',
+        preco: 70.29,
+        capa: '',
+    },
+    {
+        id: 6,
+        titulo: 'Harry Potter e o Enigma do Príncipe',
+        autor: 'J. K. Rowling ',
+        preco: 65.55,
+        capa: '',
+    },
+    {
+        id: 7,
+        titulo: 'Harry Potter e as Relíquias da Morte',
+        autor: 'J. K. Rowling',
+        preco: 80.30,
+        capa: 'https://m.media-amazon.com/images/I/81PbdwXqKkL._SY425_.jpg',
+    },
+    {
+        id: 8,
+        titulo: 'Harry Potter e a Criança Amaldiçoada',
+        autor: 'J. K. Rowling',
+        preco: 58.87,
+        capa: '',
+    }
+];
 
-const bookSearch = computed(() => state.searchTerm)
-
-function searchBook() {
-console.log('Buscando por:', bookSearch.value)
-}
 </script>
 
 <template>
@@ -18,13 +67,13 @@ console.log('Buscando por:', bookSearch.value)
     <nav>
     <div class="logo">
         <a href="#">IFbooks</a>
-        <hr />
+        <hr/>
         <p>Apreço a leitura</p>
     </div>
 
     <div class="pesquisa">
-        <input type="text" v-model="state.searchTerm" placeholder="Buscar livro..." />
-        <button @click="searchBook">🔍</button>
+        <input type="text" placeholder="Buscar livro..." />
+        <button>🔍</button>
     </div>
     <div class="menu">
         <ul>
@@ -34,14 +83,60 @@ console.log('Buscando por:', bookSearch.value)
             <li><a href="#">Devoluções</a></li>
         </ul>
         <div>
-            <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+            <ul>
+                <li><a href="#"><span class="material-symbols-outlined">search</span></a></li>
+                <li><a href="#"><span class="material-symbols-outlined">favorite</span></a></li>
+                <li><a href="#"><span class="material-symbols-outlined">person</span></a></li>
+            </ul>
         </div>
 
     </div>
     </nav>
+    <hr>
 </header>
+
+<main>
+    <section class="banner">
+    <div>
+        <p>Autor de Abril</p>
+        <h1>Eric-Emanuel Schmitt </h1>
+        <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+        <a href="#">Acessar página do livro</a>
+    </div>
+    <div>
+        <img src="../public/book.png" alt="imagem livro noc ognia">
+    </div>
+    </section>
+
+    <section class="info">
+        <hr>
+        <ul>
+            <li>
+                <span class="material-symbols-outlined">local_shipping</span>
+                <p>Frete grátis para SC</p>
+            </li>
+            <li>
+                <span class="material-symbols-outlined">star</span>
+                <p>Livros recomendados</p>
+            </li>
+            <li>
+                <span class="material-symbols-outlined">menu_book</span>
+                <p>Mais vendidos</p>
+            </li>
+        </ul>
+        <hr>
+
+    <ul v-for="livro in livros" :key="livro.id">
+        <li>
+        <img :src=" livro.capa " alt="imagem livros"></li>
+        <li>{{ livro.titulo }}</li>
+        <li>{{ livro.autor }}</li>
+        <li>R${{ livro.preco }}</li>
+        <button>Comprar</button>
+    </ul>
+    </section>
+
+</main>
 </template>
 
 <style scoped></style>
